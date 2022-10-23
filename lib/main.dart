beff495256ffb34ef90e83a01d766a91e3ac94c7
Yue-plus <Yue_plus@foxmail.com>
2022-10-23 16:28:32 +0800

    update MaterialApp i18n

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 import 'src/home.dart';
 
 const title = '点名！';
 
 void main() => runApp(MaterialApp(
-  title: title,
-  themeMode: ThemeMode.system,
-  initialRoute: '/',
-  routes: <String, WidgetBuilder> {
-    '/': (BuildContext context) => const Home(),
-  },
-  debugShowCheckedModeBanner: false,
-));
+      title: title,
+      themeMode: ThemeMode.system,
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/': (BuildContext context) => const Home(),
+      },
+      supportedLocales: const [
+        Locale.fromSubtags(
+          languageCode: 'zh',
+          scriptCode: 'Hans',
+          countryCode: 'CN',
+        ),
+      ],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
+      debugShowCheckedModeBanner: false,
+    ));
