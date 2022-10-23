f4265b9f4f3d1169ef4e7fd226a68a84eeef295c
Yue-plus <Yue_plus@foxmail.com>
2022-10-23 18:00:20 +0800

    update home.dart 主要布局

--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cue/src/students.dart';
 
 import '../main.dart';
 
@@ -10,10 +11,80 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  /// 未点到列表
+  final List<String> notArrivedList = [];
+
+  /// 已到人数
+  int arrived = 0;
+
+  /// 当前点到人数
+  int index = 0;
+
+  /// 重喊
+  void _refresh() {
+    // TODO
+  }
+
+  /// 到！
+  void _check() {
+    if (index < students.length - 1) {
+      setState(() => index++);
+    }
+  }
+
+  /// 跳过
+  void _skip() {
+    if (index < students.length - 1) {
+      notArrivedList.add(students[index]);
+      setState(() => index++);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text(title)),
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+          mainAxisSize: MainAxisSize.max,
+          children: [
+            Text(
+              '人数：$index/${students.length - 1}',
+              style: const TextStyle(fontSize: 32),
+            ),
+            const SizedBox(height: 23),
+            Text(
+              students[index],
+              style: const TextStyle(fontSize: 100, fontFamily: '楷体'),
+            ),
+            const SizedBox(height: 23),
+            Row(
+              mainAxisSize: MainAxisSize.min,
+              mainAxisAlignment: MainAxisAlignment.center,
+              textBaseline: TextBaseline.ideographic,
+              children: [
+                IconButton(
+                  onPressed: _refresh,
+                  iconSize: 64,
+                  icon: const Icon(Icons.refresh),
+                ),
+                IconButton(
+                  onPressed: _check,
+                  iconSize: 64,
+                  icon: const Icon(Icons.check),
+                ),
+                IconButton(
+                  onPressed: _skip,
+                  iconSize: 64,
+                  icon: const Icon(Icons.skip_next),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
